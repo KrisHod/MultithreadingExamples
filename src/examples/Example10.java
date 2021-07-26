82d13e53baa236da1c89f2e76364c4a5e82c742d
Kris <krisgodom@gmail.com>
2021-07-26 15:20:44 -0300

    add example with Thread pool

---
+++ src/examples/Example10.java
@@ -0,0 +1,34 @@
+package examples;
+
+/**
+ * Thread pool, Executor Service
+ */
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Example10 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5); //factory method of class Executors, created 5 threads
+        ExecutorService executorService1 = Executors.newSingleThreadExecutor(); //created Thread pool with one thread
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new RunnableImpl());
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        System.out.println("Main ends");
+    }
+}
+
+class RunnableImpl implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
